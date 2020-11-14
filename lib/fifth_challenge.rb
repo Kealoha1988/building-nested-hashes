3410c6017cd7c3bca4d790972d971eaa0cce063c
Kealoha1988 <kealohaabe1988@gmail.com>
2020-11-14 10:34:34 -0500

    commit tree f15ee1bca36d29931a43118f747e873f6821b8e4

--- lib/fifth_challenge.rb
+++ lib/fifth_challenge.rb
@@ -3,7 +3,7 @@ require "pry"
 def fifth_challenge
 
   epic_tragedy = {
-   :montague => {
+     :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
@@ -23,9 +23,9 @@ def fifth_challenge
    }
   }
 
-  #code your solution here:
-
-
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
   #Don't touch the following line! The method must return our newly modified epic tragedy hash
   epic_tragedy
 end
+
